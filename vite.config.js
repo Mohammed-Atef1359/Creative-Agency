7aabe6d2c8542831bf435c42746a86a2d5dd0f19
Your NameMohammed-Atef1359 <myworkk1359@gmail.com>
2025-10-05 12:52:27 +0300

    zzzzzzzzzzzzzzzz

--- vite.config.js
+++ vite.config.js
@@ -9,9 +9,9 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "index.html"),
         about: resolve(__dirname, "about.html"),
-        contact: resolve(__dirname, "contact.html"),
+        contact: resolve(__dirname,"contact.html"),
         project: resolve(__dirname, "project.html"),
-        services: resolve(__dirname, "services.html"),
+        services: resolve(__dirname, "services.html")
       },
     },
   },
