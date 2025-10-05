a47381455920505aebb526b64a79a2d530573d49
Your NameMohammed-Atef1359 <myworkk1359@gmail.com>
2025-10-05 12:25:39 +0300

    zzzzzzzzzzzzzzzz

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        about: resolve(__dirname, "/about.html"),
+        about: resolve(__dirname, "about.html"),
         services: resolve(__dirname, "services.html"),
         project: resolve(__dirname, "project.html"),
         contact: resolve(__dirname, "contact.html"),
